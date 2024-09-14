5798eff9f54cf167a9019361ab846f2ec3bc6943
erickgfs <erickgfs@gmail.com>
2024-09-14 02:24:47 -0300

    Estilos

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,56 @@
+import { HistoryContainer, HistoryList, Status } from './styles';
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1> History </h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
 import {
   HomeContainer,
   FormContainer,
+  TaskInput,
+  MinutesAmountInput,
   CountdownContainer,
   Separator,
+  StartCountdownButton,
 } from './styles';
 
 import { Play } from 'phosphor-react';
@@ -13,10 +16,28 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="De um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -29,10 +50,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
